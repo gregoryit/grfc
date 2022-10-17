67d979bc3d74d246d2983cd42c2f330288624cdd
gregoryit <mike25012000@gmail.com>
2022-10-17 18:09:43 +0000

    change .sql

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,6 +1,11 @@
 CREATE TABLE "users"(
     "id" INTEGER NOT NULL,
-    "sudb_id" INTEGER NOT NULL
+    "sudb_id" INTEGER NOT NULL,
+    "position_id" INTEGER NOT NULL,
+    "fio" VARCHAR(255) NOT NULL,
+    "fio_r" VARCHAR(255) NOT NULL,
+    "fio_d" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
     "users" ADD PRIMARY KEY("id");
@@ -39,12 +44,21 @@ CREATE TABLE "roles_menu"(
     "role_id" INTEGER NOT NULL,
     "menu_id" INTEGER NOT NULL
 );
+CREATE TABLE "positions"(
+    "id" INTEGER NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "description" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "positions" ADD PRIMARY KEY("id");
 ALTER TABLE
     "users_roles" ADD CONSTRAINT "users_roles_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
 ALTER TABLE
     "users_subdivisions" ADD CONSTRAINT "users_subdivisions_user_id_foreign" FOREIGN KEY("user_id") REFERENCES "users"("id");
 ALTER TABLE
     "users" ADD CONSTRAINT "users_sudb_id_foreign" FOREIGN KEY("sudb_id") REFERENCES "subdivision"("id");
+ALTER TABLE
+    "users" ADD CONSTRAINT "users_position_id_foreign" FOREIGN KEY("position_id") REFERENCES "positions"("id");
 ALTER TABLE
     "roles_menu" ADD CONSTRAINT "roles_menu_role_id_foreign" FOREIGN KEY("role_id") REFERENCES "roles"("id");
 ALTER TABLE
