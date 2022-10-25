dd43dd63ed79dd18cc32e5bef2b35b3663a778a2
gregoryit <mike25012000@gmail.com>
2022-10-25 20:17:25 +0000

    STATIC_ROOT

--- grfc_backend/grfc_backend/settings.py
+++ grfc_backend/grfc_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.228', 'gregory.fun']
+ALLOWED_HOSTS = ['192.168.1.228', 'gregory.fun', '91.214.242.240']
 
 
 # Application definition
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
