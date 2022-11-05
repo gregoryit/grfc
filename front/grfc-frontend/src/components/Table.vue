f00df744f37d679a17ebc656d0501e0bfbf2a2f4
plamyaa <sergeevm100@gmail.com>
2022-11-05 19:49:27 +0300

    added table markup

---
+++ front/grfc-frontend/src/components/Table.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="table-wrapper">
+    <TableNavigation />
+  </div>
+</template>
+
+<script lang="ts">
+import TableNavigation from './tableComponents/TableNavigation.vue';
+export default {
+  name: 'AppTable',
+  components: {
+    TableNavigation,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table-wrapper {
+  margin: 30px 10px 0;
+}
+</style>
