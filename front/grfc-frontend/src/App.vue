4a74d0ab3171ba88dd3ce2c7c562a3a29efe513d
Bogdan Glinin <twinkl31337@gmail.com>
2022-11-05 21:11:32 +0300

    2-menu-lauout

--- front/grfc-frontend/src/App.vue
+++ front/grfc-frontend/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <Header />
+  <Content />
 </template>
 
 <script lang="ts">
 import Header from './components/Header.vue';
+import Content from './components/Content.vue';
 export default {
   name: 'App',
   components: {
     Header,
+    Content,
   },
 };
 </script>
