147349564ce8058048fc7799b2ac7fe740b853ed
gregoryit <mike25012000@gmail.com>
2022-12-05 08:39:46 +0000

    vue + django integration on front

--- front/grfc-frontend/vue.config.js
+++ front/grfc-frontend/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://127.0.0.1:8081',
+  outputDir: '../../grfc_backend/static/dist',
+  indexPath: '../../templates/base-vue.html',
+  configureWebpack: {
+    devServer: {
+      devMiddleware: {
+        index: true,
+        publicPath: 'http://127.0.0.1:8081',
+        headers: {"Access-Control-Allow-Origin": "*"},
+        writeToDisk: filePath => filePath.endsWith('index.html'),
+      },
+    },
+  }
 });
