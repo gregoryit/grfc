8dd6882fed1f26142fa1e904ff213d0e0042871d
plamyaa <sergeevm100@gmail.com>
2022-12-11 22:29:40 +0300

    added image and renamed classes

--- front/grfc-frontend/src/components/headerComponents/ProfileDropdown.vue
+++ front/grfc-frontend/src/components/headerComponents/ProfileDropdown.vue
@@ -1,50 +1,53 @@
 <template>
-  <div id="notificationsDropdown__wrapper">
-    <div class="notificationsDropdown__body">
-      <button @click="$emit('toggle')" class="notificationsDropdown__close-btn">
-        <img src="../../assets/header/close_btn.svg"
-             alt="X"
-             class="notificationsDropdown__close-svg">
+  <div id="notifications-dropdown__wrapper">
+    <div class="notifications-dropdown__body">
+      <button @click="$emit('toggle')" class="notifications-dropdown__close-btn">
+        <img
+          src="../../assets/header/close_btn.svg"
+          alt="X"
+          class="notifications-dropdown__close-svg"
+        />
       </button>
-      <div class="notificationsDropdown__content">
-        <div class="notificationsDropdown__item">
-          <div class="notificationsDropdown__info">
-            <div class="notificationsDropdown__title">Название</div>
-            <div class="notificationsDropdown__time">now</div>
+      <div class="notifications-dropdown__content">
+        <div class="notifications-dropdown__item">
+          <div class="notifications-dropdown__info">
+            <div class="notifications-dropdown__title">Название</div>
+            <div class="notifications-dropdown__time">now</div>
           </div>
-          <div class="notificationsDropdown__description">
-            Описание... <br/>...</div>
-        </div>
-        <div class="notificationsDropdown__item">
-          <div class="notificationsDropdown__info">
-            <div class="notificationsDropdown__title">Название</div>
-            <div class="notificationsDropdown__time">9:30 AM</div>
-          </div>
-          <div class="notificationsDropdown__description">
+          <div class="notifications-dropdown__description">
             Описание... <br />...
           </div>
         </div>
-        <div class="notificationsDropdown__item">
-          <div class="notificationsDropdown__info">
-            <div class="notificationsDropdown__title">Название</div>
-            <div class="notificationsDropdown__time">9:25 AM</div>
+        <div class="notifications-dropdown__item">
+          <div class="notifications-dropdown__info">
+            <div class="notifications-dropdown__title">Название</div>
+            <div class="notifications-dropdown__time">9:30 AM</div>
           </div>
-          <div class="notificationsDropdown__description">
+          <div class="notifications-dropdown__description">
             Описание... <br />...
           </div>
         </div>
-        <div class="notificationsDropdown__item">
-          <div class="notificationsDropdown__info">
-            <div class="notificationsDropdown__title">Название</div>
-            <div class="notificationsDropdown__time">9:20 AM</div>
+        <div class="notifications-dropdown__item">
+          <div class="notifications-dropdown__info">
+            <div class="notifications-dropdown__title">Название</div>
+            <div class="notifications-dropdown__time">9:25 AM</div>
           </div>
-          <div class="notificationsDropdown__description">
+          <div class="notifications-dropdown__description">
             Описание... <br />...
           </div>
         </div>
-        <a href=""
-           class="notificationsDropdown__open-all"
-           target="#">Открыть все...</a>
+        <div class="notifications-dropdown__item">
+          <div class="notifications-dropdown__info">
+            <div class="notifications-dropdown__title">Название</div>
+            <div class="notifications-dropdown__time">9:20 AM</div>
+          </div>
+          <div class="notifications-dropdown__description">
+            Описание... <br />...
+          </div>
+        </div>
+        <a href="" class="notifications-dropdown__open-all" target="#"
+          >Открыть все...</a
+        >
       </div>
     </div>
   </div>
@@ -52,20 +55,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-export default defineComponent ({
+export default defineComponent({
   name: 'ProfileDropdown',
 });
 </script>
 
 <style lang="scss" scoped>
-
-#notificationsDropdown__wrapper {
+#notifications-dropdown__wrapper {
   z-index: 1000;
   position: absolute;
   top: 80px;
-  left: 1030px;
 }
-.notificationsDropdown__body {
+.notifications-dropdown__body {
   display: flex;
   flex-direction: column;
   width: 235px;
@@ -73,33 +74,30 @@ export default defineComponent ({
   background: linear-gradient(180deg, rgba(0, 8, 85, 0.6) 0%, #000855 100%);
 }
 
-.notificationsDropdown__content {
+.notifications-dropdown__content {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
   height: inherit;
   margin-top: 20px;
-
 }
 
-.notificationsDropdown__item {
+.notifications-dropdown__item {
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 215px;
   height: 70px;
   background: rgba(255, 254, 254, 0.7);
-  opacity: 0.8;
   border-radius: 10px;
 }
 
-.notificationsDropdown__info {
+.notifications-dropdown__info {
   display: flex;
   width: 198px;
   height: 24px;
   justify-content: space-between;
-  font-family: 'Roboto', sans-serif;
   font-style: normal;
   font-weight: 500;
   font-size: 12px;
@@ -108,10 +106,9 @@ export default defineComponent ({
   color: rgba(0, 0, 0, 0.8);
 }
 
-.notificationsDropdown__description {
+.notifications-dropdown__description {
   width: 199px;
   height: 30px;
-  font-family: 'Roboto', sans-serif;
   font-style: normal;
   font-weight: 400;
   font-size: 12px;
@@ -119,8 +116,7 @@ export default defineComponent ({
   color: rgba(0, 0, 0, 0.8);
 }
 
-.notificationsDropdown__open-all {
-  font-family: 'Roboto', sans-serif;
+.notifications-dropdown__open-all {
   font-style: normal;
   font-weight: 500;
   font-size: 12px;
@@ -130,7 +126,7 @@ export default defineComponent ({
   text-decoration: none;
 }
 
-.notificationsDropdown__close-btn {
+.notifications-dropdown__close-btn {
   width: 23px;
   height: 23px;
   border: 0;
