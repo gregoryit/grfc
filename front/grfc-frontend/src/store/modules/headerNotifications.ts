33538550726744b5b7d790cc2019f0afe37b4870
AndrewwKovv <kovshov.K@yandex.ru>
2022-12-14 21:09:59 +0300

    fix-Notifications

--- front/grfc-frontend/src/store/modules/headerNotifications.ts
+++ front/grfc-frontend/src/store/modules/headerNotifications.ts
@@ -40,31 +40,3 @@ export default {
     },
   },
 };
-
-//         <div class="notifications-dropdown__item">
-//           <div class="notifications-dropdown__info">
-//             <div class="notifications-dropdown__title">Название</div>
-//             <div class="notifications-dropdown__time">9:30 AM</div>
-//           </div>
-//           <div class="notifications-dropdown__description">
-//             Описание... <br />...
-//           </div>
-//         </div>
-//         <div class="notifications-dropdown__item">
-//           <div class="notifications-dropdown__info">
-//             <div class="notifications-dropdown__title">Название</div>
-//             <div class="notifications-dropdown__time">9:25 AM</div>
-//           </div>
-//           <div class="notifications-dropdown__description">
-//             Описание... <br />...
-//           </div>
-//         </div>
-//         <div class="notifications-dropdown__item">
-//           <div class="notifications-dropdown__info">
-//             <div class="notifications-dropdown__title">Название</div>
-//             <div class="notifications-dropdown__time">9:20 AM</div>
-//           </div>
-//           <div class="notifications-dropdown__description">
-//             Описание... <br />...
-//           </div>
-//         </div>
